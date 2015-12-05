3499626b77b05c2a566ae002475ccddf8bdea2be
IAmYourBrother <josephrhodiumsharp@gmail.com>
2015-12-06 03:21:48 +0800

    maraming changes.

--- CSAStudentMS.Models/PeerAdviser.cs
+++ CSAStudentMS.Models/PeerAdviser.cs
@@ -9,7 +9,8 @@ namespace CSAStudentMS.Models
     public class PeerAdviser : Student
     {
         private int renderedhours;
-        public PeerAdviser(int idnum, string name, string program, int yearlevel) : base(idnum,name, program, yearlevel)
+        public PeerAdviser(string idnum, string name, string program, int yearlevel) 
+            : base(idnum,name, program, yearlevel)  
         { }
 
         public int RenderedHours
---
+++ MD5UnitTest/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+namespace MD5UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string pass;
+            pass = GetMd5Hash("Joseph");
+            Console.WriteLine(pass);
+            Console.Read();
+        }
+        public static string GetMd5Hash(string input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                    sBuilder.Append(data[i].ToString("x2"));
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Verify a hash against a string.
+        /// </summary>
+        public static bool VerifyMd5Hash(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetMd5Hash(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            if (0 == comparer.Compare(hashOfInput, hash))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Converts the value of a SecureString to string. 
+        /// Source: http://www.codeproject.com/Tips/549109/Working-with-SecureString
+        /// </summary>
+        /// <param name="secureString"></param>
+        /// <returns></returns>
+        public static string UnsecureSecureString(SecureString secureString)
+        {
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+    }
+}
